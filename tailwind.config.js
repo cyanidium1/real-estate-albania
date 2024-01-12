0557372176c43c733bf414adb84f1d1e01ec12e7
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-01-12 01:51:54 +0100

    50%

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./!(build|dist|.*)/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ivory: "#fdfaec",
+        text2: "#002d4f",
+        "nero": "#fff",
+        text: "#000e18",
+        gray: "rgba(255, 255, 255, 0.16)",
+        khaki: "#e8d380",
+      },
+      spacing: {},
+      fontFamily: {
+        rokkitt: "Rokkitt",
+        inter: "Inter",
+        inherit: "inherit",
+        arial: "Arial",
+      },
+    },
+    fontSize: {
+      base: "16px",
+      sm: "14px",
+      xs: "12px",
+      "5xl": "24px",
+      inherit: "inherit",
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
